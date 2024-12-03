1c25e03137c653c2065502bd24178bc8e2b500b3
skyler dennis <skylerdenniss11@gmail.com>
2024-12-03 11:17:28 -0500

    I finnished the method overloading lesson

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,43 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int Add(int numOne, int numTwo)
         {
-            Console.WriteLine("Hello, World!");
+            return numOne + numTwo;
         }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne,int numTwo,bool isMoney)
+        {
+            var sum = numOne + numTwo;
+
+            if(isMoney && sum > 1)
+            {
+                return $"{sum} Dollars.";
+            }
+            else if(isMoney && sum == 1)
+            {
+                return $"{sum} Dollar.";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }    
+                
+          static void Main(string[] args)
+        {
+            Console.WriteLine(Add(7, 5,true));
+        }      
+                 
+                
+            
+        
+
+        
     }
 }
